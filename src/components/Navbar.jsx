56772e680216d5785cb964659e0160df134f678c
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-16 13:42:36 -0600

    Basic routing set up, authentication working for basic server login

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import "./Navbar.css";
+
+export const NavBar = () => {
+  const navigate = useNavigate();
+  return (
+    <ul className="navbar pb-10">
+      <li className="navbar__item pl-10">
+        <NavLink
+          className="text-left underline text-blue-600 hover:text-purple-700"
+          to={"/posts"}
+        >
+          All Posts
+        </NavLink>
+      </li>
+      <li className="navbar__item">
+        <NavLink
+          className="text-left underline text-blue-600 hover:text-purple-700"
+          to={"/myposts"}
+        >
+          My Posts
+        </NavLink>
+      </li>
+      <li className="navbar__item">
+        <NavLink
+          className="text-left underline text-blue-600 hover:text-purple-700"
+          to={"/categorymanager"}
+        >
+          Category Manager
+        </NavLink>
+      </li>
+      <li className="navbar__item">
+        <NavLink
+          className="text-left underline text-blue-600 hover:text-purple-700"
+          to={"/tagmanager"}
+        >
+          Tag Manager
+        </NavLink>
+      </li>
+      {localStorage.getItem("rare_token") !== null ? (
+        <li className="navbar__item">
+          <button
+            className="underline text-blue-600 hover:text-purple-700"
+            onClick={() => {
+              localStorage.removeItem("rare_token");
+              navigate("/login");
+            }}
+          >
+            Logout
+          </button>
+        </li>
+      ) : (
+        <>
+          <li className="navbar__item">
+            <NavLink
+              className="text-left underline text-blue-600 hover:text-purple-700"
+              to={"/login"}
+            >
+              Login
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink
+              className="text-left underline text-blue-600 hover:text-purple-700"
+              to={"/register"}
+            >
+              Register
+            </NavLink>
+          </li>
+        </>
+      )}{" "}
+    </ul>
+  );
+};
---
+++ src/pages/Register.jsx
@@ -0,0 +1,126 @@
+import React, { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.css";
+
+export const Register = () => {
+  const [username, setUsername] = useState("admina@straytor.com");
+  const [password, setPassword] = useState("straytor");
+  const [firstName, setFirstName] = useState("Admina");
+  const [lastName, setLastName] = useState("Straytor");
+  const existDialog = useRef();
+  const navigate = useNavigate();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:8000/register`, {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+        first_name: firstName,
+        last_name: lastName,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((authInfo) => {
+        if (authInfo && authInfo.token) {
+          localStorage.setItem("rare_token", JSON.stringify(authInfo));
+          navigate("/");
+        } else {
+          existDialog.current.showModal();
+        }
+      });
+  };
+
+  return (
+    <main className="container--login">
+      <dialog className="dialog dialog--auth" ref={existDialog}>
+        <div>User does not exist</div>
+        <button
+          className="button--close"
+          onClick={(e) => existDialog.current.close()}
+        >
+          Close
+        </button>
+      </dialog>
+
+      <section>
+        <form className="form--login" onSubmit={handleRegister}>
+          <h1 className="text-4xl mt-7 mb-3">rare</h1>
+          <h2 className="text-xl mb-10">Register new account</h2>
+          <fieldset className="mb-4">
+            <label htmlFor="firstName"> First name </label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(evt) => setFirstName(evt.target.value)}
+              className="form-control"
+              placeholder=""
+              required
+              autoFocus
+            />
+          </fieldset>
+          <fieldset className="mb-4">
+            <label htmlFor="lastName"> Last name </label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(evt) => setLastName(evt.target.value)}
+              className="form-control"
+              placeholder=""
+              required
+              autoFocus
+            />
+          </fieldset>
+          <fieldset className="mb-4">
+            <label htmlFor="inputUsername"> Username </label>
+            <input
+              type="text"
+              id="inputUsername"
+              value={username}
+              onChange={(evt) => setUsername(evt.target.value)}
+              className="form-control"
+              placeholder="Username"
+              required
+              autoFocus
+            />
+          </fieldset>
+          <fieldset className="mb-4">
+            <label htmlFor="inputPassword"> Password </label>
+            <input
+              type="password"
+              id="inputPassword"
+              value={password}
+              onChange={(evt) => setPassword(evt.target.value)}
+              className="form-control"
+              placeholder="Password"
+            />
+          </fieldset>
+          <fieldset>
+            <button
+              type="submit"
+              className="button p-3 rounded-md bg-blue-800 text-blue-100"
+            >
+              Register
+            </button>
+          </fieldset>
+        </form>
+      </section>
+      <div className="loginLinks">
+        <section className="link--register">
+          <Link
+            className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+            to="/login"
+          >
+            Already have an account?
+          </Link>
+        </section>
+      </div>
+    </main>
+  );
+};
