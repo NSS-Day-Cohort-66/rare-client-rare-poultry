9958fdbc8b6562a37027896998ccf54c7c1cee5b
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-20 14:19:45 -0600

    New tag form styled

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -31,18 +31,17 @@ export const Tags = () => {
     });
 
     const updatedTags = await getAllTags(); //? Refreshes state and clears label entry field
-    setAllTags(updatedTags) 
-    setNewTag({label:""})
-    ;
+    setAllTags(updatedTags);
+    setNewTag({ label: "" });
   };
 
   return (
     <div className="__tags-container__ flex flex-col w-7/12">
-      <div className="__tags-header__ text-3xl bg-cyan-800 text-white py-2 px-12 self-center rounded-t-lg">
+      <div className="__tags-header__ text-3xl bg-cyan-800 text-white py-2 px-12 self-start translate-x-2 rounded-t-lg">
         Tags
       </div>
-      <div className="__tags-list-form-container__ flex">
-        <div className="__tags-list__ flex flex-wrap gap-4 bg-cyan-950/60 border border-white/40 py-20 items-center justify-center rounded-lg">
+      <div className="__tags-list-form-container__ flex p">
+        <div className="__tags-list__ flex flex-wrap gap-4 bg-cyan-950/60 border border-white/40 py-20 items-center justify-center rounded-lg p-10">
           {allTags.map((tag) => {
             return (
               <div
@@ -54,25 +53,34 @@ export const Tags = () => {
             );
           })}
         </div>
-        <form className="__tags-form__ flex flex-col">
-          <div>Add a Tag!</div>
-          <fieldset>
-            <input
-              type="text"
-              className="__tag-label-input__"
-              value={newTag.label}
-              placeholder="Add a tag..."
-              onChange={(event) => {
-                const copy = { ...newTag };
-                copy.label = event.target.value;
-                setNewTag(copy);
+        <div className="__tag-form-container__ self-center">
+          <form className="__tags-form__ flex flex-col items-center justify-center px-4 py-8 rounded-r-xl border border-white/20 gap-4 bg-cyan-600/20">
+            <div className="__tags-form-header__ text-lg text-white font-bold">
+              Add a Tag!
+            </div>
+            <fieldset>
+              <input
+                type="text"
+                className="__tag-label-input__"
+                value={newTag.label}
+                placeholder="Add a tag..."
+                onChange={(event) => {
+                  const copy = { ...newTag };
+                  copy.label = event.target.value;
+                  setNewTag(copy);
+                }}
+              />
+            </fieldset>
+            <button
+              className="btn-edit"
+              onClick={(event) => {
+                addTag(event);
               }}
-            />
-          </fieldset>
-          <button onClick={(event) => {
-            addTag(event)
-          }}>Save Tag</button>
-        </form>
+            >
+              Save Tag
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
