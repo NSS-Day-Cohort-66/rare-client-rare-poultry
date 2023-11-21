b54b695e1aa1640289b4eed0808fd0dd15bb0635
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-21 15:04:02 -0600

    WIP

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -11,6 +11,7 @@ export const Tags = () => {
     },
   ]);
   const [newTag, setNewTag] = useState({ label: "" });
+  const [editTag, setEditTag] = useState({})
   const editModal = useRef();
   const deleteModal = useRef();
 
@@ -39,20 +40,23 @@ export const Tags = () => {
     setNewTag({ label: "" });
   };
 
+  const changeTag = async (event, id) => {
+    event.preventDefault()
+
+    await fetch(`http://localhost:8000/tags/${id}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Token ${
+          JSON.parse(localStorage.getItem("rare_token")).token
+        }`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editTag),
+    })
+  }
+
   return (
     <div className="__tags-container__ flex flex-col w-9/12 items-center">
-      <dialog
-        className="__edit-modal__ bg-sky-400/90 p-10 font-bold"
-        ref={editModal}
-      >
-        <div>Edit Modal</div>
-        <button
-          className="btn-delete"
-          onClick={() => editModal.current.close()}
-        >
-          Close Modal
-        </button>
-      </dialog>
       <div className="__tags-header__ text-3xl bg-cyan-800 text-white py-2 px-12 self-center translate-x-2 rounded-t-lg">
         Tags
       </div>
@@ -76,6 +80,21 @@ export const Tags = () => {
                 className="__tags-item-container__ bg-cyan-500/40 py-1 px-2 text-cyan-950 flex items-center justify-between w-[256px] rounded"
                 key={tag.id}
               >
+                <dialog
+                  className="__edit-modal__ bg-sky-400/90 p-10 font-bold"
+                  ref={editModal}
+                >
+                  <div>Edit Tag</div>
+                  <button className="btn-edit">Ok</button>
+                  <button
+                    className="btn-delete"
+                    onClick={(tag) => {
+
+                    }}
+                  >
+                    Cancel
+                  </button>
+                </dialog>
                 <div>
                   <button
                     onClick={() => editModal.current.showModal()}
