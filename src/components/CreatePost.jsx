d8aa8b1a5862b818e76f28c93308d0dd895948f3
Justin Miller <jrmill94@icloud.com>
2023-11-27 09:55:08 -0600

    wip

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+
 export const CreatePost = () => {
-  return <h1>Create post will go here</h1>;
+  const [newPost, setNewPost] = useState({
+    title: "",
+    image_url: "",
+    content: "",
+  });
+
+  return (
+    <form>
+      <header>
+        <h1>New Post</h1>
+      </header>
+      <fieldset>
+        <h2>Title</h2>
+        <div>
+          <input
+            title="title"
+            placeholder="post title"
+            value={newPost.title}
+            type="text"
+          ></input>
+        </div>
+      </fieldset>
+    </form>
+  );
 };
